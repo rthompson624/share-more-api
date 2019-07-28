66904e52ef7ee1843273cbf0a18ef018b9a47fd2
Richard Thompson <rthompson624@gmail.com>
2019-07-27 23:42:14 -0600

    CRUD functionality for items

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/share-more'),
+    ItemsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ItemsController } from './items.controller';
+import { ItemsService } from './items.service';
+import { ItemSchema } from './schemas/item.schema';
 
-@Module({})
+@Module({
+  imports: [MongooseModule.forFeature([{ name: 'Item', schema: ItemSchema }])],
+  controllers: [ItemsController],
+  providers: [ItemsService]
+})
 export class ItemsModule {}
